611f6d0b71c67fdc1397d0069b2ab6e94e80e06c
rajesh6334 <118992210+rajesh6334@users.noreply.github.com>
2022-11-29 20:13:54 +0530

    Add files via upload

---
+++ student pass or fail.py
@@ -0,0 +1,13 @@
+#student pass or fail
+sno=int(input('enter sno:'))
+sname=(input('enter sname:'))
+group=(input('enter group:'))
+s1=int(input('enter marks:'))
+s2=int(input('enter marks:'))
+s3=int(input('enter marks:'))
+if s1>=35 and s2>=35 and s3>=35:
+    print('pass')
+else:
+    print('fail')
+       
+        
